ed3729ee2e7513dc4fa2a71fbde68cba970acb48
dougbtv <dosmith@redhat.com>
2019-04-09 08:07:23 -0400

    Adds hack dir and a build script

---
+++ hack/build-go.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu
+cmd=whereabouts
+eval $(go env | grep -e "GOHOSTOS" -e "GOHOSTARCH")
+GOOS=${GOOS:-${GOHOSTOS}}
+GOARCH=${GOACH:-${GOHOSTARCH}}
+GOFLAGS=${GOFLAGS:-}
+GLDFLAGS=${GLDFLAGS:-}
+CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o bin/${cmd} cmd/${cmd}.go
