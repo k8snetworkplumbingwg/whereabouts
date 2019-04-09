d971b37b2bdde640867a5af2962c4411c7e38063
dougbtv <dosmith@redhat.com>
2019-04-09 14:06:05 -0400

    convert spaces to tabs

--- types/types.go
+++ types/types.go
@@ -1,38 +1,39 @@
 package types
 
 import (
-  cnitypes "github.com/containernetworking/cni/pkg/types"
-  "net"
+	cnitypes "github.com/containernetworking/cni/pkg/types"
+	"net"
 )
 
 // Net is The top-level network config - IPAM plugins are passed the full configuration
 // of the calling plugin, not just the IPAM section.
 type Net struct {
-  Name       string      `json:"name"`
-  CNIVersion string      `json:"cniVersion"`
-  IPAM       *IPAMConfig `json:"ipam"`
+	Name       string      `json:"name"`
+	CNIVersion string      `json:"cniVersion"`
+	IPAM       *IPAMConfig `json:"ipam"`
 }
 
 // IPAMConfig describes the expected json configuration for this plugin
 type IPAMConfig struct {
-  Name      string
-  Type      string            `json:"type"`
-  Routes    []*cnitypes.Route `json:"routes"`
-  Addresses []Address         `json:"addresses,omitempty"`
-  DNS       cnitypes.DNS      `json:"dns"`
+	Name      string
+	Type      string            `json:"type"`
+	Routes    []*cnitypes.Route `json:"routes"`
+	Addresses []Address         `json:"addresses,omitempty"`
+
+	DNS cnitypes.DNS `json:"dns"`
 }
 
 // IPAMEnvArgs are the environment vars we expect
 type IPAMEnvArgs struct {
-  cnitypes.CommonArgs
-  IP      cnitypes.UnmarshallableString `json:"ip,omitempty"`
-  GATEWAY cnitypes.UnmarshallableString `json:"gateway,omitempty"`
+	cnitypes.CommonArgs
+	IP      cnitypes.UnmarshallableString `json:"ip,omitempty"`
+	GATEWAY cnitypes.UnmarshallableString `json:"gateway,omitempty"`
 }
 
 // Address is our standard address.
 type Address struct {
-  AddressStr string `json:"address"`
-  Gateway    net.IP `json:"gateway,omitempty"`
-  Address    net.IPNet
-  Version    string
+	AddressStr string `json:"address"`
+	Gateway    net.IP `json:"gateway,omitempty"`
+	Address    net.IPNet
+	Version    string
 }
