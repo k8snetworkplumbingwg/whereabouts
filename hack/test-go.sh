66d16ff5a3cab1b5d4eed0dd8f967d37fb658f55
Chris Randles <randles.chris@gmail.com>
2019-10-09 18:43:34 -0400

    remove etcd from test-go

--- hack/test-go.sh
+++ hack/test-go.sh
@@ -2,20 +2,6 @@
 # single test: go test -v ./pkg/storage/
 # without cache: go test -count=1 -v ./pkg/storage/
 set -e -x
-echo "Stopping and removing etcd server..."
-docker stop etcd || true
-docker rm etcd || true
-echo "Start etcd server..."
-docker run -dt \
--p 2379:2379 \
--p 2380:2380 \
---name etcd quay.io/coreos/etcd:latest \
-/usr/local/bin/etcd \
--listen-client-urls http://0.0.0.0:2379 \
---data-dir=/etcd-data --name node1 \
---initial-advertise-peer-urls http://127.0.0.1:2380 --listen-peer-urls http://127.0.0.1:2380 \
---advertise-client-urls http://127.0.0.1:2379 \
---initial-cluster node1=http://127.0.0.1:2380
 echo "Linting go code..."
 golint ./cmd ./pkg
 echo "Running go tests..."
