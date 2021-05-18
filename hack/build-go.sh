5e8cafde59fc910f032df1e58997751920957ca4
Antonin Bas <abas@vmware.com>
2021-05-18 14:56:56 -0700

    Fix typo in hack/build-go.sh
    s/GOACH/GOARCH

--- hack/build-go.sh
+++ hack/build-go.sh
@@ -3,7 +3,7 @@ set -eu
 cmd=whereabouts
 eval $(go env | grep -e "GOHOSTOS" -e "GOHOSTARCH")
 GOOS=${GOOS:-${GOHOSTOS}}
-GOARCH=${GOACH:-${GOHOSTARCH}}
+GOARCH=${GOARCH:-${GOHOSTARCH}}
 GOFLAGS=${GOFLAGS:-}
 GLDFLAGS=${GLDFLAGS:-}
 CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o bin/${cmd} cmd/${cmd}.go
