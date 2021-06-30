34acf15a91b052e59ee44d10067d03fc4ea86c7b
Miguel Duarte Barroso <mdbarroso@redhat.com>
2021-06-30 18:35:02 +0200

    Export find IPReservation matching ID to a func
    Currently, matching the owner of the IP when deallocating the
    address happens only based on the containerID.
    
    In follow-up commits we will require this behavior to happen based
    on the pod reference attribute.
    
    This commit prepares the code base for that, by enabling the caller
    to inject the criteria for identifying the owner of an IP into the
    IP address de-allocation function.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- pkg/allocate/allocate.go
+++ pkg/allocate/allocate.go
@@ -37,7 +37,7 @@ func AssignIP(ipamConf types.IPAMConfig, reservelist []types.IPReservation, cont
 // DeallocateIP assigns an IP using a range and a reserve list.
 func DeallocateIP(reservelist []types.IPReservation, containerID string) ([]types.IPReservation, net.IP, error) {
 
-	updatedreservelist, hadip, err := IterateForDeallocation(reservelist, containerID)
+	updatedreservelist, hadip, err := IterateForDeallocation(reservelist, containerID, getMatchingIPReservationIndex)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -48,17 +48,12 @@ func DeallocateIP(reservelist []types.IPReservation, containerID string) ([]type
 }
 
 // IterateForDeallocation iterates overs currently reserved IPs and the deallocates given the container id.
-func IterateForDeallocation(reservelist []types.IPReservation, containerID string) ([]types.IPReservation, net.IP, error) {
+func IterateForDeallocation(
+	reservelist []types.IPReservation,
+	containerID string,
+	matchingFunction func(reservation []types.IPReservation, id string) int) ([]types.IPReservation, net.IP, error) {
 
-	// Cycle through and find the index that corresponds to our containerID
-	foundidx := -1
-	for idx, v := range reservelist {
-		if v.ContainerID == containerID {
-			foundidx = idx
-			break
-		}
-	}
-
+	foundidx := matchingFunction(reservelist, containerID)
 	// Check if it's a valid index
 	if foundidx < 0 {
 		return reservelist, nil, fmt.Errorf("Did not find reserved IP for container %v", containerID)
@@ -68,6 +63,17 @@ func IterateForDeallocation(reservelist []types.IPReservation, containerID strin
 
 	updatedreservelist := removeIdxFromSlice(reservelist, foundidx)
 	return updatedreservelist, returnip, nil
+}
+
+func getMatchingIPReservationIndex(reservelist []types.IPReservation, id string) int {
+	foundidx := -1
+	for idx, v := range reservelist {
+		if v.ContainerID == id {
+			foundidx = idx
+			break
+		}
+	}
+	return foundidx
 }
 
 func removeIdxFromSlice(s []types.IPReservation, i int) []types.IPReservation {
