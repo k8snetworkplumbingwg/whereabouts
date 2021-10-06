876f0a29275c97b16802433c50713b9b384e1cf8
Miguel Duarte Barroso <mdbarroso@redhat.com>
2021-10-06 14:50:19 +0200

    e2e tests: manually reconcile the cluster
    Before checking for stale IP addresses, trigger the `ip-reconciler`
    tool, which will cleanup stale IP addresses.
    
    In order to manually run the reconciler, we need to build it first;
    as such, the github action must be updated.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- hack/e2e-test.sh
+++ hack/e2e-test.sh
@@ -207,6 +207,10 @@ is_ippool_consistent() {
   local pod_ips
   local resolved_ippool_ip
   local exit_code=0
+
+  echo "Forcing reconciliation of the cluster ..."
+  bin/ip-reconciler -kubeconfig="${HOME}"/.kube/config
+
   ippool_keys=($(kubectl get ippool $pool_name --namespace $pool_namespace --output json \
     | jq --raw-output '.spec.allocations|to_entries |map("\(.key)")| .[]'))
   ips=($(nmap -sL -n $range | awk '/Nmap scan report for/{printf "%s ", $NF}'))
