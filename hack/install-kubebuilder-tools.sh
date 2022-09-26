4779b0d416b6716ae6b4e6e57e9567e87a1aa448
Konstantin Lebedev <lebedev_k@tochka.com>
2022-09-26 14:57:29 +0500

    fix make test on os type darwin

--- hack/install-kubebuilder-tools.sh
+++ hack/install-kubebuilder-tools.sh
@@ -2,17 +2,18 @@
 OCI_BIN=${OCI_BIN:-docker}
 
 BASEDIR=$(pwd)
+OSTYPE=$(uname -s | tr '[:upper:]' '[:lower:]')
 
 # install controller-gen
 GOBIN=${BASEDIR}/bin go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.1
 
 # install kubebuilder tools to bin/
 mkdir -p bin
-containerID=$("$OCI_BIN" create gcr.io/kubebuilder/thirdparty-linux:1.16.4)
-"$OCI_BIN" cp ${containerID}:/kubebuilder_linux_amd64.tar.gz ./kubebuilder_linux_amd64.tar.gz
+containerID=$("$OCI_BIN" create gcr.io/kubebuilder/thirdparty-${OSTYPE}:1.16.4)
+"$OCI_BIN" cp ${containerID}:/kubebuilder_${OSTYPE}_amd64.tar.gz ./kubebuilder_${OSTYPE}_amd64.tar.gz
 "$OCI_BIN" rm ${containerID}
-tar -xzvf kubebuilder_linux_amd64.tar.gz
-rm kubebuilder_linux_amd64.tar.gz
+tar -xzvf kubebuilder_${OSTYPE}_amd64.tar.gz
+rm kubebuilder_${OSTYPE}_amd64.tar.gz
 mv kubebuilder/bin/* bin/
 rm -rf kubebuilder/
 chmod +x bin/
