6684139632524370e24fe3baf4270611991f8ffd
nicklesimba <simha.nikhil@gmail.com>
2023-11-22 12:30:19 -0600

    Extended support for customizing whereabouts ip-reconciler cron schedule
    Signed-off-by: nicklesimba <simha.nikhil@gmail.com>

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -70,7 +70,7 @@ type IPAMConfig struct {
 	ConfigurationPath        string           `json:"configuration_path"`
 	PodName                  string
 	PodNamespace             string
-	NetworkName              string           `json:"network_name,omitempty"`
+	NetworkName              string `json:"network_name,omitempty"`
 }
 
 func (ic *IPAMConfig) UnmarshalJSON(data []byte) error {
@@ -106,7 +106,7 @@ func (ic *IPAMConfig) UnmarshalJSON(data []byte) error {
 		ConfigurationPath        string           `json:"configuration_path"`
 		PodName                  string
 		PodNamespace             string
-		NetworkName              string           `json:"network_name,omitempty"`
+		NetworkName              string `json:"network_name,omitempty"`
 	}
 
 	ipamConfigAlias := IPAMConfigAlias{
