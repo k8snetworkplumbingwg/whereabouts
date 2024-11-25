a7ae952df5802ce10e97575e5e8b2b520b026375
Benjamin Pickard <bpickard@redhat.com>
2024-11-25 18:51:45 -0500

    Skip Static test
    In order to run upstream tests in a container with make test we need to
    be able to skip the static check
    
    Signed-off-by: Benjamin Pickard <bpickard@redhat.com>

--- hack/test-go.sh
+++ hack/test-go.sh
@@ -1,20 +1,38 @@
 #!/usr/bin/env bash
 # single test: go test -v ./pkg/storage/
 # without cache: go test -count=1 -v ./pkg/storage/
-set -e -x
+set -eox pipefail
 
 GO=${GO:-go}
+SKIP_STATIC_CHECK=$false
+#parse args
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -s|--skip-static-check)
+      SKIP_STATIC_CHECK=true
+      shift
+      ;;
+    *)
+      echo "Invalid arguement: $1"
+      echo "Usage: $0 [-s|--skip-static-check]"
+      exit 1
+  esac
+done
 
 echo "Running go vet ..."
 ${GO} vet --tags=test ./cmd/... ./pkg/...
 
 BASEDIR=$(pwd)
 
-echo "Installing golang staticcheck ..."
-GOBIN=${BASEDIR}/bin go install honnef.co/go/tools/cmd/staticcheck@latest
-
-echo "Running golang staticcheck ..."
-${BASEDIR}/bin/staticcheck --tags=test ./...
+if [ $SKIP_STATIC_CHECK ]
+then
+    echo "Skipped golang staticcheck"
+else
+  echo "Installing golang staticcheck ..."
+  GOBIN=${BASEDIR}/bin go install honnef.co/go/tools/cmd/staticcheck@latest
+  echo "Running golang staticcheck ..."
+  ${BASEDIR}/bin/staticcheck --tags=test ./...
+fi
 
 echo "Running go tests..."
 KUBEBUILDER_ASSETS="$(pwd)/bin" ${GO} test \
