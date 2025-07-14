ec684977fd9e275261681a7ebf2a663a2aac376d
Marcelo <marguerr@redhat.com>
2025-07-14 14:01:53 +0200

    Run e2e node slice tests separately
    Node slice is still experimental. Its tests should not block
    regular CI.
    
    Signed-off-by: Marcelo <marguerr@redhat.com>

--- e2e/e2e_node_slice/e2e_node_slice_test.go
+++ e2e/e2e_node_slice/e2e_node_slice_test.go
@@ -3,7 +3,7 @@ package e2e_node_slice
 
 import (
 	"context"
-	"github.com/k8snetworkplumbingwg/whereabouts/pkg/api/whereabouts.cni.cncf.io/v1alpha1"
+	"testing"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -22,10 +22,16 @@ import (
 	"github.com/k8snetworkplumbingwg/whereabouts/e2e/retrievers"
 	testenv "github.com/k8snetworkplumbingwg/whereabouts/e2e/testenvironment"
 	"github.com/k8snetworkplumbingwg/whereabouts/e2e/util"
+	"github.com/k8snetworkplumbingwg/whereabouts/pkg/api/whereabouts.cni.cncf.io/v1alpha1"
 	"github.com/k8snetworkplumbingwg/whereabouts/pkg/storage"
 	wbstorage "github.com/k8snetworkplumbingwg/whereabouts/pkg/storage/kubernetes"
 	"github.com/k8snetworkplumbingwg/whereabouts/pkg/types"
 )
+
+func TestWhereaboutsE2ENodeSlice(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "whereabouts-e2e-node-slice")
+}
 
 var _ = Describe("Whereabouts node slice functionality", func() {
 	Context("Test setup", func() {
