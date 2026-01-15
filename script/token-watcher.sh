26f0072abbd5786e64f9dec0a4536e3d752696c0
Jonathan Gayvallet <jonathan.mael.gayvallet@gmail.com>
2026-01-15 23:06:19 +0100

    fix token-watcher script

--- script/token-watcher.sh
+++ script/token-watcher.sh
@@ -10,12 +10,12 @@ while true; do
   # Check the md5sum of the service account token and ca.
   svcaccountsum="$(get_token_md5sum)"
   casum="$(get_ca_file_md5sum)"
-  if [ "$svcaccountsum" != "$LAST_SERVICEACCOUNT_MD5SUM" ] || ! [ "$SKIP_TLS_VERIFY" == "true" ] && [ "$casum" != "$LAST_KUBE_CA_FILE_MD5SUM" ]; then
+  if [ "$svcaccountsum" != "$LAST_SERVICEACCOUNT_MD5SUM" ] || ([ "$SKIP_TLS_VERIFY" != "true" ] && [ "$casum" != "$LAST_KUBE_CA_FILE_MD5SUM" ]); then
     log "Detected service account or CA file change, regenerating kubeconfig..."
     generateKubeConfig
     LAST_SERVICEACCOUNT_MD5SUM="$svcaccountsum"
     LAST_KUBE_CA_FILE_MD5SUM="$casum"
   fi
 
-  sleep 1s
+  sleep 1
 done
